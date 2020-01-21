d77a9756d1fe50da3fce0756e3ecb548f5782e78
ViniEspinosaDev <viniespinosa,developer@gmail.com>
2020-01-21 20:39:10 -0300

    Cadastro Vendedor

--- WoopiWebMVC/Controllers/SellersController.cs
+++ WoopiWebMVC/Controllers/SellersController.cs
@@ -3,14 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WoopiWebMVC.Models;
+using WoopiWebMVC.Services;
 
 namespace WoopiWebMVC.Controllers
 {
     public class SellersController : Controller
     {
+        private readonly SellerService _sellerService; 
+
+        public SellersController(SellerService sellerService)
+        {
+            _sellerService = sellerService;
+        }
+
         public IActionResult Index()
         {
+            var list = _sellerService.FindAll();
+            return View(list);
+        }
+
+        public IActionResult Create()
+        {
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
---
+++ WoopiWebMVC/Services/SellerService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WoopiWebMVC.Data;
+using WoopiWebMVC.Models;
+
+namespace WoopiWebMVC.Services
+{
+    public class SellerService
+    {
+        private readonly WoopiWebMVCContext _context;
+
+        public SellerService(WoopiWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
+    }
+}
