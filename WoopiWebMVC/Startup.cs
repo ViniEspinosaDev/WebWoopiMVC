38eb7469932db1584bff4062f761b2fe23958bd1
ViniEspinosaDev <viniespinosa,developer@gmail.com>
2020-01-20 23:11:34 -0300

    Bugs Corrigidos - Banco de dados Criado

--- WoopiWebMVC/Startup.cs
+++ WoopiWebMVC/Startup.cs
@@ -37,7 +37,8 @@ namespace WoopiWebMVC
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<WoopiWebMVCContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("WoopiWebMVCContext")));
+                options.UseMySql(Configuration.GetConnectionString("WoopiWebMVCContext"), builder =>
+                    builder.MigrationsAssembly("WoopiWebMVC")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
