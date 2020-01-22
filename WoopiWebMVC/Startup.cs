d77eff4d80c2ebcdc779ea5e02628d4d6cc2b445
ViniEspinosaDev <viniespinosa,developer@gmail.com>
2020-01-21 22:31:48 -0300

    Formatando campos de texto

--- WoopiWebMVC/Startup.cs
+++ WoopiWebMVC/Startup.cs
@@ -10,8 +10,10 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Localization;
 using WoopiWebMVC.Data;
 using WoopiWebMVC.Services;
+using System.Globalization;
 
 namespace WoopiWebMVC
 {
@@ -49,6 +51,16 @@ namespace WoopiWebMVC
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
+            var enUS = new CultureInfo("en-US");
+            var localizationOption = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUS),
+                SupportedCultures = new List<CultureInfo> { enUS },
+                SupportedUICultures = new List<CultureInfo> { enUS }
+            };
+
+            app.UseRequestLocalization(localizationOption);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -70,6 +82,11 @@ namespace WoopiWebMVC
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+        }
+
+        private void localizationOptions(RequestLocalizationOptions obj)
+        {
+            throw new NotImplementedException();
         }
     }
 }
