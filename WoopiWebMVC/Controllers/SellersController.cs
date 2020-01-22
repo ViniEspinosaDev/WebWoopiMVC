8fae5b783008bd6e9a2d52bc2b769e7a51c7f95d
ViniEspinosaDev <viniespinosa,developer@gmail.com>
2020-01-21 21:04:47 -0300

    SellerFormViewModel

--- WoopiWebMVC/Controllers/SellersController.cs
+++ WoopiWebMVC/Controllers/SellersController.cs
@@ -5,16 +5,18 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WoopiWebMVC.Models;
 using WoopiWebMVC.Services;
+using WoopiWebMVC.Models.ViewModels;
 
 namespace WoopiWebMVC.Controllers
 {
     public class SellersController : Controller
     {
-        private readonly SellerService _sellerService; 
-
-        public SellersController(SellerService sellerService)
+        private readonly SellerService _sellerService;
+        private readonly DepartmentService _departmentService;
+        public SellersController(SellerService sellerService, DepartmentService departmentService)
         {
             _sellerService = sellerService;
+            _departmentService = departmentService;
         }
 
         public IActionResult Index()
@@ -25,7 +27,9 @@ namespace WoopiWebMVC.Controllers
 
         public IActionResult Create()
         {
-            return View();
+            var departments = _departmentService.FindAll();
+            var ViewModel = new SellerFormViewModel { Departments = departments };
+            return View(ViewModel);
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
---
+++ WoopiWebMVC/Services/DepartmentService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WoopiWebMVC.Data;
+using WoopiWebMVC.Models;
+
+namespace WoopiWebMVC.Services
+{
+    public class DepartmentService
+    {
+        private readonly WoopiWebMVCContext _context;
+
+        public DepartmentService(WoopiWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
