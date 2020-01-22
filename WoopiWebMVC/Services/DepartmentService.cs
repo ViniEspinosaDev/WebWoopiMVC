4c7248f7e3e7773fbb772d1de54ef0191b3cf4db
ViniEspinosaDev <viniespinosa,developer@gmail.com>
2020-01-21 23:23:04 -0300

    Tratamento de excecao

--- WoopiWebMVC/Services/DepartmentService.cs
+++ WoopiWebMVC/Services/DepartmentService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using WoopiWebMVC.Data;
 using WoopiWebMVC.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace WoopiWebMVC.Services
 {
@@ -16,9 +17,9 @@ namespace WoopiWebMVC.Services
             _context = context;
         }
 
-        public List<Department> FindAll()
+        public async Task<List<Department>> FindAllAsync()
         {
-            return _context.Department.OrderBy(x => x.Name).ToList();
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
         }
     }
 }
